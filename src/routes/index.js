66e00e0740394888d01276e33361deca9f673c32
NgocAnhsne <93851582+NgocAnhsne@users.noreply.github.com>
2024-03-14 14:28:09 +0700

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 
-import Home from '../Home/index.js';
-import Login from '../../pages/Login/index.js';
-import Signup from '../../pages/Signup/index.js';
-import Profile from '../../pages/Profile/index.js';
-import Upload from '../../pages/Upload/index.js';
-import CreateTestTeacher from '../CreateTestTeacher/index.js';
+import Home from '../pages/Home/index.js';
+import Login from '../pages/Login/index.js';
+import Signup from '../pages/Signup/index.js';
+import Profile from '../pages/Profile/index.js';
+import Upload from '../pages/Upload/index.js';
+import CreateTestTeacher from '../pages/CreateTestTeacher/index.js';
 import AnswerStudent from '~/pages/AnswerStudent/index.js';
 
 const publicRoutes = [
