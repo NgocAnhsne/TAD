e2f5a70d63e1fd7da181401cd234bba5ef55566f
TienDat <95626306+LMTTienDat@users.noreply.github.com>
2024-03-14 12:32:13 +0700

    update sidebar

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,10 +1,13 @@
-import Sidebar from "./Sidebar/index";
+import SidebarAdmin from "./Sidebar/SidebarAdmin";
+import SidebarStudent from "./Sidebar/SidebarStudent";
+import SidebarTeacher from "./Sidebar/SidebarTeacher/index";
+import "~/components/GlobalStyles/GlobalStyles.scss"
 
 function DefaultLayout({children}) {
     return ( 
         <div>
             <div className="container">
-                <Sidebar/>
+                <SidebarAdmin/>
                 <div className="content">
                     {children}
                 </div>
