b6ce8eac4b478258a590b07fbe45fd9cb7ee5bf0
NgocAnhsne <ngocanhne2402@gmail.com>
2024-03-27 15:47:49 +0700

    create history

---
+++ src/pages/Student/History/index.js
@@ -0,0 +1,18 @@
+import "style.scss";
+
+function HistoryStudent() {
+    return ( <
+        div className = "historyStudent" >
+        <
+        div className = "historyStudent__background" >
+        <
+        div className = "historyStudent__title" >
+
+        <
+        /div> <
+        /div> <
+        /div>
+    );
+}
+
+export default HistoryStudent;
