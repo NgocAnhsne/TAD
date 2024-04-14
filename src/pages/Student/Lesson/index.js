de3e26ee24fb4553a806e7cf577c6f503a6205e1
NgocAnhsne <ngocanhne2402@gmail.com>
2024-04-14 21:23:57 +0700

    lesson

--- src/pages/Student/Lesson/index.js
+++ src/pages/Student/Lesson/index.js
@@ -1,8 +1,37 @@
 import "./style.scss";
-import lessonImg from "~/components/asset/img/quality_restoration_20240229152.jpg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function LessonStudent() {
-  
+import lessonImg from "~/components/asset/img/quality_restoration_20240229152.jpg";
+import { Link } from "react-router-dom";
+
+export default function LessonStudent() {
+  const [isVisibleLoading, setIsVisibleLoading] = useState(false);
+
+  const [lessionData, setLessionData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const result = await axios("http://127.0.0.1:8000/api/lession/all");
+      setLessionData(result.data.data);
+    } catch (err) {
+      console.log("somthing Wrong");
+    }
+  };
+
+  const handleDelete = async (id) => {
+    console.log(id);
+    await axios.delete("http://127.0.0.1:8000/api/lession/delete/" + id);
+    const newListData = lessionData.filter((item) => {
+      alert("Đã xoá danh mục");
+      return item.id !== id;
+    });
+    setLessionData(newListData);
+  };
+  var moment = require("moment");
   return (
     <div className="lessonStudent">
       <div className="lessonStudent__title">
@@ -10,14 +39,54 @@ function LessonStudent() {
       </div>
       <div className="lessonStudent__content">
         <div className="lessonStudent__content--list">
+          {lessionData.length > 0 ? (
+            lessionData.map((lession, i) => (
+              <div className="lessonStudent__content--list__item">
+                <div className="lessonStudent__content--list__item--top">
+                  <div className="lessonStudent__content--list__item--top__left">
+                    <div className="lessonStudent__content--list__item--top__left--title">
+                      {lession.name}
+                    </div>
+                    <div className="lessonStudent__content--list__item--top__left--body">
+                      <span>Description: {lession.description}</span>
+                      <span>{lession.time} minutes</span>
+                      <span>
+                        Type :<span> {lession.type}</span>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="lessonStudent__content--list__item--top__right">
+                    <img src={lessonImg} />
+                  </div>
+                </div>
+                  <div className="lessonStudent__content--list__item--bottom">
+                  <Link className="lessonStudent__content--list__item--bottom__btnPlay lessonBtn" to={`/student/answer`}>
+                    Xem thêm
+                    </Link>
+                    <div className="lessonStudent__content--list__item--bottom__btnDel lessonBtn" onClick={() => handleDelete(lession.id)}>
+                    Xóa
+                    </div>
+                    
+                  </div>
+              </div>
+            ))
+          ) : (
+            <div>
+              <h1 className="text-danger text-center">
+                Không tìm thấy bộ đề nào
+              </h1>
+            </div>
+          )}
+        </div>
+      </div>
+      {/* <div className="lessonStudent__content">
+        <div className="lessonStudent__content--list">
           <div className="lessonStudent__content--list__item">
             <div className="lessonStudent__content--list__item--left">
               <div className="lessonStudent__content--list__item--left__title">
                 Bài học cơ bản cho người mất gốc
               </div>
-              <div className="lessonStudent__content--list__item--left__btn">
-                Xem thêm
-              </div>
+              
             </div>
             <div className="lessonStudent__content--list__item--right">
               <img src={lessonImg}/>
@@ -50,9 +119,7 @@ function LessonStudent() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
-
-export default LessonStudent;
