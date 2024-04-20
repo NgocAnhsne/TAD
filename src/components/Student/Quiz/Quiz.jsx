f77bf67184e5ed73507f7e284225706e76df0765
NgocAnhsne <ngocanhne2402@gmail.com>
2024-04-21 01:00:58 +0700

    Update Quiz.jsx

--- src/components/Student/Quiz/Quiz.jsx
+++ src/components/Student/Quiz/Quiz.jsx
@@ -13,6 +13,8 @@ const Quiz = () => {
     let [score, setScore] = useState(0);
     let [result, setResult] = useState(false);
     const [ans, setAns] = useState([]);
+    const [questionLength, setQuestionLength] = useState(0);
+    const[currentQuestion,setCurrentQuestion] = useState();
     let Option1 = useRef(null);
     let Option2 = useRef(null);
     let Option3 = useRef(null);
@@ -20,7 +22,6 @@ const Quiz = () => {
 
     let option_array = [Option1, Option2, Option3, Option4];
     // const questions = Array.from({ length: numberOfQuestions }, (_, index) => index + 1);
-
     //data
     const { id } = useParams();
     useEffect(() => {
@@ -30,17 +31,31 @@ const Quiz = () => {
     const fetchData = async () => {
         try {
             const res = await axios.get("http://127.0.0.1:8000/api/question-by-lession/" + id);
-            setQuestion(res.data.data);
-            setAns(res.data.data);
-            console.log(ans)
+            if(res.data.data){
+                setQuestion(res.data.data);
+                setCurrentQuestion(res.data.data[index])
+                setQuestionLength(res.data.data.length);
+            }
         } catch (err) {
             console.log("Something Wrong");
         }
     }
-    const checkAns = (answer) => {
+
+    const checkAns = async (event, answer) => {
         if (lock === false && question.length > 0) {
-            const newAnsData = question.filter((item) => item.answer == ans);
-            console.log(newAnsData);
+            const newAnsData = await question.find((item) => item.answer == answer);
+            if(!!newAnsData){
+                event.target.classList.add("correct");
+                setLock(true);
+                setScore(prev => prev + 1);
+                return;
+            }
+                event.target.classList.add("wrong");
+            
+                setLock(true)
+                // option_array[question.answer - 1].current.classList.add("correct"); // Đánh dấu câu trả lời đúng
+            return ;
+            // console.log(newAnsData);
             // if (question.answer == newAnsData) {
             //     e.target.classList.add("correct");
             //     setLock(true);
@@ -55,13 +70,14 @@ const Quiz = () => {
     }
 
     const next = () => {
+        // console.log({question});
         if (lock === true) {
             if (index === question.length - 1) {
                 setResult(true);
                 return 0;
             }
             setIndex(++index);
-            setQuestion();
+            setCurrentQuestion(question[index]);
             setLock(false);
             option_array.map((option) => {
                 option.current.classList.remove("wrong");
@@ -71,12 +87,15 @@ const Quiz = () => {
         }
     }
 
+    const handleSendResult = () => {
+        // console.log({question});
+        
+        console.log("You have submitted your assignment successfully")
+    }
+
 
 
     // var moment = require('moment');
-
-
-    const questionLength = question.length;
     return (
         <><div className="answerStudent">
             <div className="answerStudent__header">
@@ -94,8 +113,6 @@ const Quiz = () => {
                     </div>
                 </div>
             </div>
-            {question.map((question) =>
-            (
                 <div className="answerStudent__content">
                     <div className="answerStudent__content--top">
                         <div className="answerStudent__content--top__img">
@@ -106,7 +123,7 @@ const Quiz = () => {
 
                                 <div className="answerStudent__content--qtop__question--box__text">
                                     {/* ______ going to have to work hard to achieve your goals. */}
-                                    {index + 1}. {question.question_text}
+                                    {index + 1}. {currentQuestion?.question_text}
                                 </div>
                             </div>
                         </div>
@@ -115,34 +132,35 @@ const Quiz = () => {
                     <div className="answerStudent__content--bottom">
                         <div className="answerStudent__content--bottom__list">
                             <div className="answerStudent__content--bottom__list--item">
-                                <button ref={Option1} onClick={(e) => { checkAns(e, 1); }}>A. {question.answer_a}</button>
+                                <button ref={Option1} onClick={(e) => checkAns(e, "a")}>A. {currentQuestion?.answer_a}</button>
                             </div>
                             <div className="answerStudent__content--bottom__list--item">
-                                <button ref={Option2} onClick={(e) => { checkAns(e, 2); }}>B. {question.answer_b}</button>
+                                <button ref={Option2} onClick={(e) => { checkAns(e, "b"); }}>B. {currentQuestion?.answer_b}</button>
                             </div>
                             <div className="answerStudent__content--bottom__list--item">
-                                <button ref={Option3} onClick={(e) => { checkAns(e, 3); }}>C. {question.answer_c}</button>
+                                <button ref={Option3} onClick={(e) => { checkAns(e, "c"); }}>C. {currentQuestion?.answer_c}</button>
                             </div>
                             <div className="answerStudent__content--bottom__list--item">
-                                <button ref={Option4} onClick={(e) => { checkAns(e, 4); }}>D. {question.answer_d}</button>
+                                <button ref={Option4} onClick={(e) => { checkAns(e, "d"); }}>D. {currentQuestion?.answer_d}</button>
                             </div>
                         </div>
                         <div className="answerStudent__content--bottom__btnNext">
+                            {
+                            index !== question.length - 1
+                            ?
                             <button onClick={next}>
                                 Next
                             </button>
+                            :
+                            <button onClick={handleSendResult}>
+                                Send result
+                            </button>
+                            }
                         </div>
                     </div>
-
                 </div>
-
-            ))}
-
-
         </div><div>
-
             </div></>
-
     )
 }
 export default Quiz;
